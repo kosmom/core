4ed3ce9aa852982bb13f95ea56398bc616e620ee
Pogodin Konstantin <kosmom@kosmom.ru>
2020-09-14 09:25:51 +0300

    dbwork bigint numbers check fix

--- dbwork.php
+++ dbwork.php
@@ -405,7 +405,7 @@ class dbwork{
 					case 'tinyint':
 					case 'bigint':
 					case 'mediumint':
-					if ($value != intval($value)){
+						if(!preg_match('#^\d+$#',$value)){
 						$outErrors[]=translate::t('Field {field} have number format. You try set <b>{value}</b> value',array('field'=>$fieldName,'value'=>input::htmlspecialchars($value)));
 						break;
 					}
