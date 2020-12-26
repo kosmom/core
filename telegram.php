7b8b70ce904da6b77b9a5a19d4cf2f09a41d2e08
Pogodin Konstantin <kosmom@kosmom.ru>
2020-12-26 22:59:01 +0300

    telegram oauth check feature

--- telegram.php
+++ telegram.php
@@ -134,4 +134,19 @@ class telegram{
 			return \json_decode($rs, \true);
 		}
 	}
+	static function checkAuth($auth_data){
+		$check_hash = $auth_data['hash'];
+		unset($auth_data['hash']);
+		$data_check_arr = [];
+		foreach ($auth_data as $key => $value) {
+			$data_check_arr[] = $key.'='.$value;
+		}
+		\sort($data_check_arr);
+		$data_check_string = \implode("\n", $data_check_arr);
+		$secret_key = \hash('sha256', core::$data['telegram'], true);
+		$hash = \hash_hmac('sha256', $data_check_string, $secret_key);
+		if (\strcmp($hash, $check_hash) !== 0)return \false;
+		if ((\time() - $auth_data['auth_date']) > 86400)return \false;
+		return \true;
+	}
 }
