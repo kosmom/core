c441fc1232e20917fc0d2209f772baffe77d45ef
Pogodin Konstantin <kosmom@kosmom.ru>
2025-05-21 16:17:17 +0300

    filedata compact + cache invalidate on save

--- filedata.php
+++ filedata.php
@@ -6,65 +6,63 @@ namespace c;
  * @author Kosmom <Kosmom.ru>
  */
 class filedata{
-	static $mime_types = array(
-		'txt' => 'text/plain',
-		'htm' => 'text/html',
-		'html' => 'text/html',
-		'php' => 'text/html',
-		'css' => 'text/css',
-		'js' => 'application/javascript',
-		'json' => 'application/json',
-		'xml' => 'application/xml',
-		'swf' => 'application/x-shockwave-flash',
-		'flv' => 'video/x-flv',
+	static $mime_types=array(
+		'txt'=>'text/plain',
+		'htm'=>'text/html',
+		'html'=>'text/html',
+		'php'=>'text/html',
+		'css'=>'text/css',
+		'js'=>'application/javascript',
+		'json'=>'application/json',
+		'xml'=>'application/xml',
+		'swf'=>'application/x-shockwave-flash',
+		'flv'=>'video/x-flv',
 
 		// images
-		'png' => 'image/png',
-		'jpe' => 'image/jpeg',
-		'jpeg' => 'image/jpeg',
-		'jpg' => 'image/jpeg',
-		'gif' => 'image/gif',
-		'bmp' => 'image/bmp',
-		'ico' => 'image/vnd.microsoft.icon',
-		'tiff' => 'image/tiff',
-		'tif' => 'image/tiff',
-		'svg' => 'image/svg+xml',
-		'svgz' => 'image/svg+xml',
+		'png'=>'image/png',
+		'jpe'=>'image/jpeg',
+		'jpeg'=>'image/jpeg',
+		'jpg'=>'image/jpeg',
+		'gif'=>'image/gif',
+		'bmp'=>'image/bmp',
+		'ico'=>'image/vnd.microsoft.icon',
+		'tiff'=>'image/tiff',
+		'tif'=>'image/tiff',
+		'svg'=>'image/svg+xml',
+		'svgz'=>'image/svg+xml',
 
 		// archives
-		'zip' => 'application/zip',
-		'rar' => 'application/x-rar-compressed',
-		'exe' => 'application/x-msdownload',
-		'msi' => 'application/x-msdownload',
-		'cab' => 'application/vnd.ms-cab-compressed',
+		'zip'=>'application/zip',
+		'rar'=>'application/x-rar-compressed',
+		'exe'=>'application/x-msdownload',
+		'msi'=>'application/x-msdownload',
+		'cab'=>'application/vnd.ms-cab-compressed',
 
 		// audio/video
-		'mp3' => 'audio/mpeg',
-		'qt' => 'video/quicktime',
-		'mov' => 'video/quicktime',
+		'mp3'=>'audio/mpeg',
+		'qt'=>'video/quicktime',
+		'mov'=>'video/quicktime',
 
 		// adobe
-		'pdf' => 'application/pdf',
-		'psd' => 'image/vnd.adobe.photoshop',
-		'ai' => 'application/postscript',
-		'eps' => 'application/postscript',
-		'ps' => 'application/postscript',
+		'pdf'=>'application/pdf',
+		'psd'=>'image/vnd.adobe.photoshop',
+		'ai'=>'application/postscript',
+		'eps'=>'application/postscript',
+		'ps'=>'application/postscript',
 
-		// ms office
-		'doc' => 'application/msword',
-		'docx' => 'application/msword',
-		'rtf' => 'application/rtf',
-		'xls' => 'application/vnd.ms-excel',
-		'xlsx' => 'application/vnd.ms-excel',
-		'ppt' => 'application/vnd.ms-powerpoint',
-		'pptx' => 'application/vnd.ms-powerpoint',
-
-		// open office
-		'odt' => 'application/vnd.oasis.opendocument.text',
-		'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+		// office
+		'doc'=>'application/msword',
+		'docx'=>'application/msword',
+		'rtf'=>'application/rtf',
+		'xls'=>'application/vnd.ms-excel',
+		'xlsx'=>'application/vnd.ms-excel',
+		'ppt'=>'application/vnd.ms-powerpoint',
+		'pptx'=>'application/vnd.ms-powerpoint',
+		'odt'=>'application/vnd.oasis.opendocument.text',
+		'ods'=>'application/vnd.oasis.opendocument.spreadsheet',
 	);
 	
-	static $dataPartHandlers = array();
+	static $dataPartHandlers=array();
 	
 	/**
 	 * Get stored data from file
@@ -72,7 +70,6 @@ class filedata{
 	 * @return array
 	 */
 	static function loaddata($filename){
-		if (\function_exists('opcache_invalidate'))\opcache_invalidate($filename);
 		return @include $filename;
 	}
 
@@ -82,19 +79,20 @@ class filedata{
 	 * @param array $data
 	 */
 	static function savedata($filename,$data){
-		return \file_put_contents($filename,'<?php return '.self::varExportMin($data).';');
+		$rs=\file_put_contents($filename,'<?php return '.self::varExportMin($data).';');
+		if ($rs && \function_exists('opcache_invalidate'))\opcache_invalidate($filename);
+		return $rs;
 	}
 
-	private static function varExportMin($var) {
-		if (\is_array($var)) {
-			$toImplode = array();
-			foreach ($var as $key => $value) {
-				$toImplode[] = \var_export($key, \true).'=>'.self::varExportMin($value);
+	private static function varExportMin($var){
+		if (\is_array($var)){
+			$toImplode=array();
+			foreach ($var as $key=>$value){
+				$toImplode[]=\var_export($key,\true).'=>'.self::varExportMin($value);
 			}
-			return 'array('.\implode(',', $toImplode).')';
-		} else {
-			return \var_export($var, 1);
+			return 'array('.\implode(',',$toImplode).')';
 		}
+		return \var_export($var, 1);
 	}
 	/**
 	 * Recursive remove directory
@@ -119,7 +117,7 @@ class filedata{
 	 */
 	static function empt($dir){
 		if (!\is_dir($dir))return true;
-		foreach (\array_diff(\scandir($dir), array('.','..')) as $file) {
+		foreach (\array_diff(\scandir($dir),array('.','..')) as $file){
 			\is_dir($dir.'/'.$file)?self::rmdir($dir.'/'.$file):\unlink($dir.'/'.$file);
 		}
 	}
@@ -137,13 +135,13 @@ class filedata{
 	 */
 	static function filelist($src_dir,$mask='',$callback=\null){
 		if (!\is_dir($src_dir))return \false;
-		$dir = \opendir($src_dir);
+		$dir=\opendir($src_dir);
 		$out=array();
-		while(\false!==($file=\readdir($dir))) {
-			if (( $file == '.' ) || ( $file == '..' ))continue;
+		while(\false!==($file=\readdir($dir))){
+			if (( $file == '.' )||( $file == '..' ))continue;
 			if (\is_dir($src_dir.\DIRECTORY_SEPARATOR.$file)){
 				$out+=self::filelist($src_dir.\DIRECTORY_SEPARATOR.$file,$mask,$callback);
-			}elseif (!$mask || \preg_match($mask, $file)){
+			}elseif (!$mask||\preg_match($mask,$file)){
 				$out[$src_dir.\DIRECTORY_SEPARATOR.$file]=\is_callable($callback)?$callback($src_dir.\DIRECTORY_SEPARATOR.$file,$file,$src_dir):\true;
 			}
 		}
@@ -158,11 +156,11 @@ class filedata{
 	 */
 	static function copy($src,$dst){
 		if (!\is_dir($src))return \false;
-		$dir = \opendir($src);
+		$dir=\opendir($src);
 		@\mkdir($dst);
 		while(\false!==($file=\readdir($dir))){
-			if (($file == '.') || ($file == '..' ))continue;
-			if (\is_dir($src.\DIRECTORY_SEPARATOR.$file)) {
+			if (($file=='.')||($file=='..'))continue;
+			if (\is_dir($src.\DIRECTORY_SEPARATOR.$file)){
 				self::copy($src.\DIRECTORY_SEPARATOR.$file,$dst.\DIRECTORY_SEPARATOR.$file);
 			} else {
 				\copy($src.\DIRECTORY_SEPARATOR.$file,$dst.\DIRECTORY_SEPARATOR.$file);
@@ -174,7 +172,7 @@ class filedata{
 	static function is_empty_dir($dir){
 		$h=\opendir($dir);
 		if (!$h)return false;
-		while (false !== ($e=\readdir($h))){
+		while(false!==($e=\readdir($h))){
 			if ($e!="." && $e!=".."){
 				\closedir($h);
 				return false;
@@ -182,7 +180,7 @@ class filedata{
 		}
 		\closedir($h);
 		return true;
-	      }
+	}
 	
 	/**
 	 * Get extension of filename
@@ -190,7 +188,7 @@ class filedata{
 	 * @return string
 	 */
 	static function extension($filename){
-		return \pathinfo($filename, \PATHINFO_EXTENSION);
+		return \pathinfo($filename,\PATHINFO_EXTENSION);
 	}
 	static function readfile($path,$filename,$attachment=\true,$type=\null){
 		if (!\file_exists($path))throw new \Exception('file not exists');
@@ -210,28 +208,28 @@ class filedata{
 		if ($type=self::$mime_types[self::extension($filename)])return $type;
 		return \false;
 	}
-	static function appendDataPart($filename, $data){
-		$pos = @\filesize($filename);
-		$append = \json_encode($data, \JSON_PRETTY_PRINT | \JSON_UNESCAPED_UNICODE);
-		$appendLen = \strlen($append);
-		error::log($filename, $append.$appendLen.\strlen($appendLen));
+	static function appendDataPart($filename,$data){
+		$pos=@\filesize($filename);
+		$append=\json_encode($data,\JSON_PRETTY_PRINT|\JSON_UNESCAPED_UNICODE);
+		$appendLen=\strlen($append);
+		error::log($filename,$append.$appendLen.\strlen($appendLen));
 	}
-	static function readLastDataPart($handlerOrPath, $offset = \null){
-		$handler = self::getHandler($handlerOrPath, $offset);
-		\fseek($handler, -1,\ftell($handler) ? \SEEK_CUR : \SEEK_END);
-		$char = (int) \fgetc($handler);
-		\fseek($handler, -1 - $char, \SEEK_CUR);
-		$pos = (int) \fgets($handler, $char + 1);
-		\fseek($handler, -$char - $pos, \SEEK_CUR);
-		$val = \fread($handler, $pos);
-		\fseek($handler, -$pos - 1, \SEEK_CUR);
+	static function readLastDataPart($handlerOrPath,$offset=\null){
+		$handler=self::getHandler($handlerOrPath,$offset);
+		\fseek($handler,-1,\ftell($handler)?\SEEK_CUR:\SEEK_END);
+		$char=(int)\fgetc($handler);
+		\fseek($handler,-1-$char,\SEEK_CUR);
+		$pos=(int)\fgets($handler,$char+1);
+		\fseek($handler,-$char-$pos,\SEEK_CUR);
+		$val=\fread($handler,$pos);
+		\fseek($handler,-$pos-1,\SEEK_CUR);
 		return $val;
 	}
 	static function getHandler($handlerOrPath,$offset=\null){
 		if (!\is_string($handlerOrPath))return $handlerOrPath;
 		if (isset(self::$dataPartHandlers[$handlerOrPath]))return self::$dataPartHandlers[$handlerOrPath];
-		self::$dataPartHandlers[$handlerOrPath]=\fopen($handlerOrPath, 'r');
-		if ($offset)\fseek(self::$dataPartHandlers[$handlerOrPath], $offset);
+		self::$dataPartHandlers[$handlerOrPath]=\fopen($handlerOrPath,'r');
+		if ($offset)\fseek(self::$dataPartHandlers[$handlerOrPath],$offset);
 		return self::$dataPartHandlers[$handlerOrPath];
 	}
 	
@@ -248,7 +246,7 @@ class filedata{
 			\touch($file);
 			\chmod($file,0777);
 			$handler=self::getHandler($file);
-			if (\flock($handler, \LOCK_EX | \LOCK_NB)){
+			if (\flock($handler,\LOCK_EX|\LOCK_NB)){
 				return $file;
 			}
 		}
