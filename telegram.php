c80bc0dc5996f32cf93561a0c44e5f0c9a683bdc
Pogodin Konstantin <kosmom@kosmom.ru>
2026-06-04 16:09:05 +0300

    telegram editDocument sendGallery fix

--- telegram.php
+++ telegram.php
@@ -23,14 +23,14 @@ class telegram{
 	const EMOJI_WHITE_CHECK_MARK=emoji::WHITE_CHECK_MARK;
 	const EMOJI_COP=emoji::COP;
 	const EMOJI_MEN_DETECTIVE=emoji::MEN_DETECTIVE;
-	
+
 	const PARSE_MODE_MARKDOWN='Markdown';
 	const PARSE_MODE_MARKDOWN2='MarkdownV2';
 	const PARSE_MODE_HTML='HTML';
 	const PARSE_MODE_NONE=null;
-	
+
 	const OPTION_PROTECT_CONTENT='protect_content';
-	
+
 	static $parse_mode;
 	
 	/**
@@ -40,65 +40,64 @@ class telegram{
 	 * @param array $data - additional request parameters
 	 * @return array
 	 */
-	static function send($method, $chat_id, $data = array()){
-		self::check();
-		$data['chat_id']=$chat_id;
-		if (isset($data['text']) && is_string($data['text'])){
-			$data['text']=input::iconv($data['text'],true);
+	static function send($method,$chat_id,$data=array()){
+	    self::check();
+	    $data['chat_id']=$chat_id;
+	    if (isset($data['text']) && \is_string($data['text']))$data['text']=input::iconv($data['text'],true);
+	    if (isset($data['caption']) && \is_string($data['caption']))$data['caption']=input::iconv($data['caption'],true);
+	    if (self::$parse_mode && !isset($data['parse_mode']))$data['parse_mode']=self::$parse_mode;
+
+	    $options=array();
+	    $hasLocalFiles=false;
+
+	    foreach (array('photo','audio','document','video','voice','video_note') as $field){
+		if (isset($data[$field]) && \is_string($data[$field]) && \is_file($data[$field])){
+		    $data[$field]=\curl_file_create(\realpath($data[$field]));
+		    $hasLocalFiles=true;
 		}
-		if (isset($data['caption']) && is_string($data['caption'])){
-			$data['caption']=input::iconv($data['caption'],true);
-		}
-		if (self::$parse_mode && !isset($data['parse_mode'])){
-			$data['parse_mode']=self::$parse_mode;
-		}
-		
-		$options=array();
-		$hasLocalFiles=false;
-		
-		foreach (array('photo','audio','document','video','voice','video_note') as $field){
-			if (isset($data[$field]) && is_string($data[$field]) && is_file($data[$field])){
-				$data[$field]=curl_file_create(realpath($data[$field]));
-				$hasLocalFiles=true;
-			}
-		}
-		
-		if (isset($data['media']) && is_array($data['media'])){
+	    }
+
+	    if (isset($data['media']) && \is_array($data['media'])){
 			foreach ($data['media'] as $index=>&$mediaItem){
-				if (isset($mediaItem['media']) && is_string($mediaItem['media']) && is_file($mediaItem['media'])){
-					$attachName='file_'.$index;
-					$originalFile=$mediaItem['media'];
-					$mediaItem['media']='attach://'.$attachName;
-					$data[$attachName]=curl_file_create(realpath($originalFile));
-					$hasLocalFiles=true;
+				if (isset($mediaItem['media']) && \is_string($mediaItem['media']) && \is_file($mediaItem['media'])){
+				$attachName='file_'.$index;
+				$originalFile=$mediaItem['media'];
+				$mediaItem['media']='attach://'.$attachName;
+				$data[$attachName]=\curl_file_create(\realpath($originalFile));
+				$hasLocalFiles=true;
 				}
 			}
 			unset($mediaItem);
-		}
-		if (isset($data['reply_markup']) && is_array($data['reply_markup'])){
-			$data['reply_markup']=json_encode($data['reply_markup']);
-		}
-		if (isset($data['inline_keyboard'])){
-			$data['reply_markup']=json_encode(array('inline_keyboard'=>$data['inline_keyboard']));
-			unset($data['inline_keyboard']);
-		}elseif (isset($data['keyboard'])){
-			$keyboardOptions=array('keyboard'=>$data['keyboard']);
-			if (isset($data['resize_keyboard']))$keyboardOptions['resize_keyboard']=$data['resize_keyboard'];
-			if (isset($data['one_time_keyboard']))$keyboardOptions['one_time_keyboard']=$data['one_time_keyboard'];
-			$data['reply_markup']=json_encode($keyboardOptions);
-			unset($data['keyboard'],$data['resize_keyboard'],$data['one_time_keyboard']);
-		}elseif (isset($data['remove_keyboard']) && $data['remove_keyboard']){
-			$data['reply_markup']=json_encode(array('remove_keyboard'=>true));
-			unset($data['remove_keyboard']);
-		}elseif (isset($data['force_reply']) && $data['force_reply']){
-			$data['reply_markup']=json_encode(array('force_reply'=>true));
-			unset($data['force_reply']);
-		}
-		if ($hasLocalFiles){
+			$data['media']=\json_encode($data['media']);
+	    }
+	    if (isset($data['reply_markup'])){
+			if (is_array($data['reply_markup'])){
+				$data['reply_markup']=\json_encode($data['reply_markup']);
+			}
+			unset($data['inline_keyboard'],$data['keyboard'],$data['resize_keyboard'],$data['one_time_keyboard'],$data['remove_keyboard'],$data['force_reply']);
+	    }else{
+			if (isset($data['inline_keyboard'])){
+				$data['reply_markup']=\json_encode(array('inline_keyboard'=>$data['inline_keyboard']));
+				unset($data['inline_keyboard']);
+			}elseif (isset($data['keyboard'])){
+				$keyboardOptions=array('keyboard'=>$data['keyboard']);
+				if (isset($data['resize_keyboard']))$keyboardOptions['resize_keyboard']=$data['resize_keyboard'];
+				if (isset($data['one_time_keyboard']))$keyboardOptions['one_time_keyboard']=$data['one_time_keyboard'];
+				$data['reply_markup']=\json_encode($keyboardOptions);
+				unset($data['keyboard'],$data['resize_keyboard'],$data['one_time_keyboard']);
+			}elseif (isset($data['remove_keyboard']) && $data['remove_keyboard']){
+				$data['reply_markup']=\json_encode(array('remove_keyboard'=>true));
+				unset($data['remove_keyboard']);
+			}elseif (isset($data['force_reply']) && $data['force_reply']){
+				$data['reply_markup']=\json_encode(array('force_reply'=>true));
+				unset($data['force_reply']);
+			}
+	    }
+	    if ($hasLocalFiles){
 			$options[CURLOPT_HTTPHEADER]=array("Content-Type"=>"multipart/form-data");
 			$options[CURLOPT_SAFE_UPLOAD]=true;
-		}
-		return input::iconv(self::request($method,$data,$options));
+	    }
+	    return input::iconv(self::request($method,$data,$options));
 	}
 	
 	static function getMe(){
@@ -111,7 +110,7 @@ class telegram{
 	}
 	static function getFileContent($fileId){
 		$rs=self::getFile($fileId);
-		if ($rs['ok']!=true)throw new Exception('file get error');
+		if ($rs['ok']!=true)throw new \Exception('file get error');
 		return self::request($rs['result']['file_path'],null,array(),true);
 	}
 	static function sendLocation($chat_id,$latitude,$longitude,$reply_markup=null){
@@ -160,7 +159,6 @@ class telegram{
 		if ($reply_markup)$data['reply_markup']=$reply_markup;
 		if ($protect_content)$data['protect_content']=true;
 		if ($reply_to_message_id)$data['reply_to_message_id']=$reply_to_message_id;
-		
 		return self::send('sendMessage',$chat_id,$data);
 	}
 	static function editMessage($chat_id,$message_id,$text,$reply_markup=null){
@@ -172,10 +170,17 @@ class telegram{
 		return self::send('deleteMessage',$chat_id,array('message_id'=>$message_id));
 	}
 	static function editDocument($chat_id,$message_id,$link,$caption=null,$reply_markup=null){
-		$data=array('message_id'=>$message_id,'document'=>$link);
-		if ($reply_markup)$data['reply_markup']=$reply_markup;
-		if ($caption)$data['caption']=$caption;
-		return self::send('editMessageMedia',$chat_id,$data);
+	    return self::send('editMessageMedia',$chat_id,\array_filter([
+		'message_id'=>$message_id,
+		'media'=>\json_encode(\array_filter([
+		    'type'=>'document',
+		    'media'=>\is_file($link)?'attach://document':$link,
+		    'caption'=>$caption,
+		    'parse_mode'=>self::$parse_mode?:null
+		])),
+		'document'=>\is_file($link)?$link:null,
+		'reply_markup'=>$reply_markup
+	    ]));
 	}
 	static function sendDocument($chat_id,$link,$caption=null,$reply_markup=null,$protect_content=false){
 		$data=array('document'=>$link);
@@ -191,6 +196,7 @@ class telegram{
 	static function sendChatAction($chat_id,$action){
 		return self::send('sendChatAction',$chat_id,array('action'=>$action));
 	}
+	
 	static function banChatMember($chat_id,$user_id,$until_date=null,$revoke_messages=null){
 		$data=array('user_id'=>$user_id);
 		if ($until_date)$data['until_date']=input::strtotime($until_date);
@@ -198,13 +204,16 @@ class telegram{
 		return self::send('banChatMember',$chat_id,$data);
 	}
 	static function sendMediaGroup($chat_id,$mediaArray,$caption=null,$protect_content=false){
-		$data=array('media'=>$mediaArray);
-		if ($caption && isset($data['media'][0])){
-			$data['media'][0]['caption']=$caption;
-			if (self::$parse_mode)$data['media'][0]['parse_mode']=self::$parse_mode;
-		}
-		if ($protect_content)$data['protect_content']=true;
-		return self::send('sendMediaGroup',$chat_id,$data);
+	    foreach ($mediaArray as $k=>&$v){
+			if (\is_string($v))$v=array('type'=>'photo','media'=>$v);
+	    }
+	    if ($caption && isset($mediaArray[0])){
+			$mediaArray[0]['caption']=input::iconv($caption,true);
+			if (self::$parse_mode)$mediaArray[0]['parse_mode']=self::$parse_mode;
+	    }
+	    $data=array('media'=>$mediaArray);
+	    if ($protect_content)$data['protect_content']=true;
+	    return self::send('sendMediaGroup',$chat_id,$data);
 	}
 	static function pinChatMessage($chat_id,$message_id,$disable_notification=false){
 		$data=array('message_id'=>$message_id);
@@ -228,7 +237,7 @@ class telegram{
 	static function checkResponse($rs){
 		if (!$rs['ok'])throw new \Exception($rs['description'],$rs['error_code']);
 		return $rs;
-	}
+		}
 	static function sendMessageOrFault($chat_id,$text,$reply_markup=null){
 		$rs=self::sendMessage($chat_id,$text,$reply_markup);
 		return self::checkResponse($rs);
@@ -253,13 +262,13 @@ class telegram{
 	 * @return array
 	 */
 	static function getData(){
-		return json_decode(input::iconv(file_get_contents("php://input")),true);
+		return \json_decode(input::iconv(\file_get_contents("php://input")),true);
 	}
 	static function check(){
 		if (!isset(core::$data['telegram']))throw new \Exception('need set c\\core::$data[\'telegram\'] key from BotFather');
 	}
 	private static function request($api,$post=null,$options=array(),$is_file=false){
-		if (is_callable(@core::$data['telegram_request'])){
+		if (\is_callable(@core::$data['telegram_request'])){
 			$a=core::$data['telegram_request'];
 			return $a($api,$post,$is_file);
 		}
@@ -268,7 +277,7 @@ class telegram{
 		if (@core::$data['telegram_proxy']['auth'])$options[CURLOPT_PROXYUSERPWD]=core::$data['telegram_proxy']['auth'];
 		if (@core::$data['telegram_proxy']['type'])$options[CURLOPT_PROXYTYPE]=core::$data['telegram_proxy']['type'];
 		$rs=curl::getContentSafely($url,$post,null,$options);
-		return $is_file?$rs:json_decode($rs,true);
+		return $is_file?$rs:\json_decode($rs,true);
 	}
 	static function checkAuth($auth_data){
 		$check_hash=$auth_data['hash'];
@@ -277,11 +286,11 @@ class telegram{
 		foreach ($auth_data as $key=>$value){
 			$data_check_arr[]=$key.'='.$value;
 		}
-		sort($data_check_arr);
-		$data_check_string=implode("\n",$data_check_arr);
-		$secret_key=hash('sha256',core::$data['telegram'],true);
-		$hash=hash_hmac('sha256',$data_check_string,$secret_key);
-		if (strcmp($hash,$check_hash)!==0)return false;
-		return $auth_data['auth_date']>=time()-86400;
+		\sort($data_check_arr);
+		$data_check_string=\implode("\n",$data_check_arr);
+		$secret_key=\hash('sha256',core::$data['telegram'],true);
+		$hash=\hash_hmac('sha256',$data_check_string,$secret_key);
+		if (\strcmp($hash,$check_hash)!==0)return false;
+		return $auth_data['auth_date']>=\time()-86400;
 	}
 }
